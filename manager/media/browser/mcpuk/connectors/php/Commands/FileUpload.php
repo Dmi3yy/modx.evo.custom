d5b43befb139111810100afc4ab4d81e1f6fa7b8
dmi3yy <dmi3yy@gmail.com>
2011-10-27 16:50:19 +0300

    транслитерация файлов при загрузке

--- manager/media/browser/mcpuk/connectors/php/Commands/FileUpload.php
+++ manager/media/browser/mcpuk/connectors/php/Commands/FileUpload.php
@@ -23,7 +23,7 @@ class FileUpload {
 	var $cwd;
 	var $actual_cwd;
 	var $newfolder;
-
+	
 	function FileUpload($fckphp_config,$type,$cwd) {
 		$this->fckphp_config=$fckphp_config;
 		$this->type=$type;
@@ -31,7 +31,52 @@ class FileUpload {
 		$this->actual_cwd=str_replace("//","/",($this->fckphp_config['UserFilesPath']."/$type/".$this->raw_cwd));
 		$this->real_cwd=str_replace("//","/",($this->fckphp_config['basedir']."/".$this->actual_cwd));
 	}
-
+	
+	function cleanFilename($filename) {
+		$n_filename="";
+		
+		//Check that it only contains valid characters
+		for($i=0;$i<strlen($filename);$i++) if (in_array(substr($filename,$i,1),$this->fckphp_config['FileNameAllowedChars'])) $n_filename.=substr($filename,$i,1);
+		
+		//If it got this far all is ok
+		return $n_filename;
+	}
+function niceFilename($filename) {
+    $changes = array(
+        "Є"=>"EH", "І"=>"I", "і"=>"i", "№"=>"#", "є"=>"eh",
+        "А"=>"A", "Б"=>"B", "В"=>"V", "Г"=>"G", "Д"=>"D",
+        "Е"=>"E", "Ё"=>"E", "Ж"=>"ZH", "З"=>"Z", "И"=>"I",
+        "Й"=>"J", "К"=>"K", "Л"=>"L", "М"=>"M", "Н"=>"N",
+        "О"=>"O", "П"=>"P", "Р"=>"R", "С"=>"S", "Т"=>"T",
+        "У"=>"U", "Ф"=>"F", "Х"=>"H", "Ц"=>"C", "Ч"=>"CH",
+        "Ш"=>"SH", "Щ"=>"SCH", "Ъ"=>"", "Ы"=>"Y", "Ь"=>"",
+        "Э"=>"E", "Ю"=>"YU", "Я"=>"YA", "Ē"=>"E", "Ū"=>"U",
+        "Ī"=>"I", "Ā"=>"A", "Š"=>"S", "Ģ"=>"G", "Ķ"=>"K",
+        "Ļ"=>"L", "Ž"=>"Z", "Č"=>"C", "Ņ"=>"N", "ē"=>"e",
+        "ū"=>"u", "ī"=>"i", "ā"=>"a", "š"=>"s", "ģ"=>"g",
+        "ķ"=>"k", "ļ"=>"l", "ž"=>"z", "č"=>"c", "ņ"=>"n",
+        "а"=>"a", "б"=>"b", "в"=>"v", "г"=>"g", "д"=>"d",
+        "е"=>"e", "ё"=>"e", "ж"=>"zh", "з"=>"z", "и"=>"i",
+        "й"=>"j", "к"=>"k", "л"=>"l", "м"=>"m", "н"=>"n",
+        "о"=>"o", "п"=>"p", "р"=>"r", "с"=>"s", "т"=>"t",
+        "у"=>"u", "ф"=>"f", "х"=>"h", "ц"=>"c", "ч"=>"ch",
+        "ш"=>"sh", "щ"=>"sch", "ъ"=>"", "ы"=>"y", "ь"=>"",
+        "э"=>"e", "ю"=>"yu", "я"=>"ya", "Ą"=>"A", "Ę"=>"E",
+        "Ė"=>"E", "Į"=>"I", "Ų"=>"U", "ą"=>"a", "ę"=>"e",
+        "ė"=>"e", "į"=>"i", "ų"=>"u", "ö"=>"o", "Ö"=>"O",
+        "ü"=>"u", "Ü"=>"U", "ä"=>"a", "Ä"=>"A", "õ"=>"o",
+        "Õ"=>"O", "є"=>"e", "Є"=>"e", "ї"=>"yi", "Ї"=>"yi", 
+		"і"=>"i", "І"=>"i", "ґ"=>"g", "Ґ"=>"g");
+    $alias=strtr($filename, $changes);
+    $alias = strtolower( $alias );
+    $alias = preg_replace('/&.+?;/', '', $alias); // kill entities
+    $alias = str_replace( '_', '-', $alias );
+    $alias = preg_replace('/[^a-z0-9\s-.]/', '', $alias);
+    $alias = preg_replace('/\s+/', '-', $alias);
+    $alias = preg_replace('|-+|', '-', $alias);
+    $alias = trim($alias, '-');
+    return $alias;
+}
 	function run() {
 		//If using CGI Upload script, get file info and insert into $_FILE array
 		if 	(
@@ -48,7 +93,10 @@ class FileUpload {
 				$disp="202,'Incomplete file information from upload CGI'";
 			}
 		}
-
+		
+// 		if (isset($_FILES['NewFile'])&&isset($_FILES['NewFile']['name'])&&($_FILES['NewFile']['name']!=""))
+// 			$_FILES['NewFile']['name']=$_FILES['NewFile']['name']; //$this->cleanFilename($_FILES['NewFile']['name']);
+		
 		$typeconfig=$this->fckphp_config['ResourceAreas'][$this->type];
 		
 		header ("content-type: text/html");
@@ -56,12 +104,7 @@ class FileUpload {
 			if (array_key_exists("NewFile",$_FILES)) {
 				if ($_FILES['NewFile']['size']<($typeconfig['MaxSize']*1024)) {
 
-					$filename=basename(str_replace("\\","/",$_FILES['NewFile']['name']));
-					//if($this->modx->config['clean_uploaded_filename']) {
-					//	$nameparts = explode('.', $filename);
-					//	array_map(array($this->modx, 'stripAlias'), $nameparts);
-					//	$filename = implode($nameparts);
-					//}
+					$filename=basename(str_replace("\\","/",$this->niceFilename($_FILES['NewFile']['name'])));
 					
 					$lastdot=strrpos($filename,".");
 					
