dc36d44fff46a171822e758422e74b92689effbe
Dmi3yy <dmi3yy@gmail.com>
2013-02-02 14:56:29 +0200

    fix update utility

--- install/instprocessor.php
+++ install/instprocessor.php
@@ -524,7 +524,7 @@ if (isset ($_POST['module']) || $installData) {
                 $rs = mysql_query("SELECT * FROM $dbase.`" . $table_prefix . "site_modules` WHERE name='$name'", $sqlParser->conn);
                 if (mysql_num_rows($rs)) {
                     $row = mysql_fetch_assoc($rs);
-                    $props = mysql_real_escape_string(propUpdate($properties,$row['properties']);
+                    $props = mysql_real_escape_string(propUpdate($properties,$row['properties']));
                     if (!@ mysql_query("UPDATE $dbase.`" . $table_prefix . "site_modules` SET modulecode='$module', description='$desc', properties='$props', enable_sharedparams='$shared' WHERE name='$name';", $sqlParser->conn)) {
                         echo "<p>" . mysql_error() . "</p>";
                         return;
