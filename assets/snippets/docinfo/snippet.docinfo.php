6c8dc9d27797dde040a2fcbaf5a060c250e02031
Dmi3yy <dmi3yy@gmail.com>
2013-02-07 19:53:19 +0200

    update docinfo

--- assets/snippets/docinfo/snippet.docinfo.php
+++ assets/snippets/docinfo/snippet.docinfo.php
@@ -1,23 +1,14 @@
 <?php
 /* 
 	*	Returns any document field or template variable from any document
-	*	[[DocInfo? &docid=`15` &field=`pagetitle`]] - Для стандартных полей ресурса
-	*	[[DocInfo? &docid=`10` &tv=`1` &field=`tvname`]] - Для TV-параметров ресурса добавляем &tv=`1`
+	*	[!DocInfo? &id=`20`!] — заголовок ресурса с id 20
+   *  [!DocInfo? &id=`20` &info=`introtext`!] — содержимое introtext ресурса с id 20
+   *  [!DocInfo? &id=`20` &tv=`price`!] — содержимое tv price ресурса с id 20
 	*/
 if(!defined('MODX_BASE_PATH')){die('What are you doing? Get out of here!');}
-$docid = (isset($docid) && (int)$docid>0) ? (int)$docid : $modx->documentIdentifier;
-$field = (isset($field)) ? $field : 'pagetitle';
-$output='';
-if(isset($tv) && $tv==1){
-   $tv=$modx->getTemplateVar($field,'*',$docid,1);
-   if($tv['value']!=''){
-      $output=$tv['value'];
-   }else{
-      $output=$tv['defaultText'];
-   }
-}else{
-   $doc=$modx->getPageInfo($docid,'1',$field);
-   $output=$doc[$field];
-}
-return $output;
+(isset($id)) ? $id : $id = $modx->documentIdentifier;
+(isset($info)) ? $info : $info = 'pagetitle';
+(isset($tv)) ? $info = $tv : '';
+$o = (isset($tv)) ? $modx->getTemplateVarOutput($info, $id) : $modx->getDocument($id,$info);
+return $o[$info];
 ?>
