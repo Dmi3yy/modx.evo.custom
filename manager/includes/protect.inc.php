b932bbab8490e7fe10ccf634fac8196b3dcc61ab
Dmi3yy <dmi3yy@gmail.com>
2013-03-17 16:16:02 +0200

    Better modx_sanitize_gpc - 3rd try

--- manager/includes/protect.inc.php
+++ manager/includes/protect.inc.php
@@ -22,43 +22,49 @@ if (@ ini_get('register_globals')) {
 }
 
 // sanitize array
-$modxtags = array('[[', ']]', '[!', '!]', '[*', '*]', '[(', ')]', '{{', '}}', '[+', '+]', '[~', '~]', '[^', '^]');
 if (!function_exists('modx_sanitize_gpc')) {
-    function modx_sanitize_gpc(&$target, $tags, $count = 0) {
-        foreach ($tags as $_) {
-            $replaced[] = " {$_['0']} {$_['1']} ";
-        }
-        foreach ($target as $key => $value) {
-            if (is_array($value)) {
+    function modx_sanitize_gpc(&$target, $dummy = array(), $count = 0) {
+        $tags = array('[', ']', '{', '}');
+        $replaced = array('&#x005B;', '&#x005D;', '&#x007B;', '&#x007D;');
+
+        $keys = array_keys($target);
+        $values = array_values($target);
+
+        for ($i = 0; $i < count($values); $i++) {
+            $key = str_replace($tags, $replaced, $keys[$i]);
+            $key = preg_replace('/<script/i', 'sanitized<s cript', $key);
+            $key = preg_replace('/&#(\d{1,4});?/', 'sanitized& #$1', $key);
+            $keys[$i] = $key;
+            if (is_array($values[$i])) {
                 $count++;
                 if (10 < $count) {
                     echo '<h1>Error: array nested too deep!</h1>';
                     exit;
                 }
-                modx_sanitize_gpc($value, $tags, $count);
+                modx_sanitize_gpc($values[$i], $tags, $count);
             } else {
-                $value = str_replace($tags, $replaced, $value);
+                $value = str_replace($tags, $replaced, $values[$i]);
                 $value = preg_replace('/<script/i', 'sanitized<s cript', $value);
                 $value = preg_replace('/&#(\d{1,4});?/', 'sanitized& #$1', $value);
-                $value = preg_replace('/&#x([0-9a-f]{1,4});?/i', 'sanitized& #x$1', $value);
-                $target[$key] = $value;
+                $values[$i] = $value;
             }
             $count = 0;
         }
-        return $target;
+
+        $target = array_combine($keys, $values);
     }
 }
 
-modx_sanitize_gpc($_GET, $modxtags);
+modx_sanitize_gpc($_GET);
 if (!defined('IN_MANAGER_MODE') || (defined('IN_MANAGER_MODE') && (!IN_MANAGER_MODE || IN_MANAGER_MODE == 'false'))) {
-    modx_sanitize_gpc($_POST, $modxtags);
+    modx_sanitize_gpc($_POST);
 }
-modx_sanitize_gpc($_COOKIE, $modxtags);
-modx_sanitize_gpc($_REQUEST, $modxtags);
+modx_sanitize_gpc($_COOKIE);
+modx_sanitize_gpc($_REQUEST);
 
 foreach (array ('PHP_SELF', 'HTTP_USER_AGENT', 'HTTP_REFERER', 'QUERY_STRING') as $key) {
     $_SERVER[$key] = isset ($_SERVER[$key]) ? htmlspecialchars($_SERVER[$key], ENT_QUOTES) : null;
 }
 
 // Unset vars
-unset ($modxtags, $key, $value);
+unset ($key, $value);
