e5368fe7ba481ec8c0f6a8a4d9bf7d02ffc90fe7
Dmi3yy <dmi3yy@gmail.com>
2013-03-19 14:27:48 +0200

    fix Youtube button on tinyMCE  full view

--- assets/plugins/tinymce/tinymce.functions.php
+++ assets/plugins/tinymce/tinymce.functions.php
@@ -101,15 +101,15 @@ class TinyMCE
 			$buttons3 = '';
 		    break;
 		case 'advanced':
-			$plugins  = '';
+			$plugins  = 'youtubeIframe';
 			$buttons1 = 'bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect';
-			$buttons2 = 'bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code';
+			$buttons2 = 'bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,anchor,image,youtubeIframe,cleanup,help,code';
 			$buttons3 = 'hr,removeformat,visualaid,|,sub,sup,|,charmap';
 		    break;
 		case 'full':
 			$plugins  = 'pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,youtubeIframe,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave,youtubeIframe';
 			$buttons1 = 'save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect';
-			$buttons2 = 'cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor';
+			$buttons2 = 'cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,youtubeIframe,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor';
 			$buttons3 = 'tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen';
 			$buttons4 = 'insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak,restoredraft';
 		    break;
