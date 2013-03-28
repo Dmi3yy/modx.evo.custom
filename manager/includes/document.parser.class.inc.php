1b748f3ff29724ece17dac4a2e94ddab823b983b
Dmi3yy <dmi3yy@gmail.com>
2013-03-29 00:38:13 +0200

    интеграция кеш экстендера

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -1352,35 +1352,67 @@ class DocumentParser {
         return ($dir != '' ? "$dir/" : '') . $pre . $alias . $suff;
     }
 
-    /** 
-     * Convert URL tags [~...~] to URLs
-     *
-     * @param string $documentSource
-     * @return string
-     */
-    function rewriteUrls($documentSource) {
-        // rewrite the urls
-        if ($this->config['friendly_urls'] == 1) {
-            $aliases= array ();
-            foreach ($this->aliasListing as $item) {
-                $aliases[$item['id']]= (strlen($item['path']) > 0 ? $item['path'] . '/' : '') . $item['alias'];
+    /* интеграция кеш экстендера */
+    if($modx->config['cacheextender'] == 1){
+        var $cacheExtender = true; //modified by thebat053
+        function rewriteUrls($documentSource) {
+            // rewrite the urls
+            if ($this->config['friendly_urls'] == 1) {
+                $in= '!\[\~([0-9]+)\~\]!ise'; // Use preg_replace with /e to make it evaluate PHP
+                preg_match_all ($in, $documentSource, $matches);
+                $aliases= array ();
+                foreach ($matches[1] as $match){
+                    $item = $this->aliasListing[$match];
+                    $aliases[$match]= (strlen($item['path']) > 0 ? $item['path'] . '/' : '') . $item['alias'];
+                }
+                $isfriendly= ($this->config['friendly_alias_urls'] == 1 ? 1 : 0);
+                $pref= $this->config['friendly_url_prefix'];
+                $suff= $this->config['friendly_url_suffix'];
+                $thealias= '$aliases[\\1]';
+                $found_friendlyurl= "\$this->makeFriendlyURL('$pref','$suff',$thealias)";
+                $not_found_friendlyurl= "\$this->makeFriendlyURL('$pref','$suff','" . '\\1' . "')";
+                $out= "({$isfriendly} && isset({$thealias}) ? {$found_friendlyurl} : {$not_found_friendlyurl})";
+                $documentSource= preg_replace($in, $out, $documentSource);
+            } else {
+                $in= '!\[\~([0-9]+)\~\]!is';
+                $out= "index.php?id=" . '\1';
+                $documentSource= preg_replace($in, $out, $documentSource);
             }
-            $in= '!\[\~([0-9]+)\~\]!ise'; // Use preg_replace with /e to make it evaluate PHP
-            $isfriendly= ($this->config['friendly_alias_urls'] == 1 ? 1 : 0);
-            $pref= $this->config['friendly_url_prefix'];
-            $suff= $this->config['friendly_url_suffix'];
-            $thealias= '$aliases[\\1]';
-            $found_friendlyurl= "\$this->makeFriendlyURL('$pref','$suff',$thealias)";
-            $not_found_friendlyurl= "\$this->makeFriendlyURL('$pref','$suff','" . '\\1' . "')";
-            $out= "({$isfriendly} && isset({$thealias}) ? {$found_friendlyurl} : {$not_found_friendlyurl})";
-            $documentSource= preg_replace($in, $out, $documentSource);
-        } else {
-            $in= '!\[\~([0-9]+)\~\]!is';
-            $out= "index.php?id=" . '\1';
-            $documentSource= preg_replace($in, $out, $documentSource);
+            return $documentSource;
         }
-        return $documentSource;
+    }else{
+        /** 
+         * Convert URL tags [~...~] to URLs
+         *
+         * @param string $documentSource
+         * @return string
+         */
+        function rewriteUrls($documentSource) {
+            // rewrite the urls
+            if ($this->config['friendly_urls'] == 1) {
+                $aliases= array ();
+                foreach ($this->aliasListing as $item) {
+                    $aliases[$item['id']]= (strlen($item['path']) > 0 ? $item['path'] . '/' : '') . $item['alias'];
+                }
+                $in= '!\[\~([0-9]+)\~\]!ise'; // Use preg_replace with /e to make it evaluate PHP
+                $isfriendly= ($this->config['friendly_alias_urls'] == 1 ? 1 : 0);
+                $pref= $this->config['friendly_url_prefix'];
+                $suff= $this->config['friendly_url_suffix'];
+                $thealias= '$aliases[\\1]';
+                $found_friendlyurl= "\$this->makeFriendlyURL('$pref','$suff',$thealias)";
+                $not_found_friendlyurl= "\$this->makeFriendlyURL('$pref','$suff','" . '\\1' . "')";
+                $out= "({$isfriendly} && isset({$thealias}) ? {$found_friendlyurl} : {$not_found_friendlyurl})";
+                $documentSource= preg_replace($in, $out, $documentSource);
+            } else {
+                $in= '!\[\~([0-9]+)\~\]!is';
+                $out= "index.php?id=" . '\1';
+                $documentSource= preg_replace($in, $out, $documentSource);
+            }
+            return $documentSource;
+        }
     }
+
+
     
     /**
      * Get all db fields and TVs for a document/resource
@@ -1752,43 +1784,77 @@ class DocumentParser {
         return $parents;
     }
 
-    /**
-     * Returns an array of child IDs belonging to the specified parent.
-     *
-     * @param int $id The parent resource/document to start from
-     * @param int $depth How many levels deep to search for children, default: 10
-     * @param array $children Optional array of docids to merge with the result.
-     * @return array Contains the document Listing (tree) like the sitemap
-     */
-    function getChildIds($id, $depth= 10, $children= array ()) {
-
-        // Initialise a static array to index parents->children
-        static $documentMap_cache = array();
-        if (!count($documentMap_cache)) {
-            foreach ($this->documentMap as $document) {
-                foreach ($document as $p => $c) {
-                    $documentMap_cache[$p][] = $c;
-                }
+    /* интеграция кеш экстендера */
+    if($modx->config['cacheextender'] == 1){
+        function getChildIds($id, $depth = 11, $children = array(), $strictchilds = null){
+            if(!is_array($id))
+                $id = array($id);
+            foreach($id as $chid){
+                $children += $this->getChildIdsRec($chid, $depth, array(), $strictchilds);
             }
+
+            return $children;
         }
 
-        // Get all the children for this parent node
-        if (isset($documentMap_cache[$id])) {
-            $depth--;
+        function getChildIdsRec($id, $depth, $children = array(), $strictchilds = null) {
+            // Get all the children for this parent node
+            if (isset($this->documentMap_cache[$id])) {
+                $depth--;
+                $tmp = $this->documentMap_cache[$id];
+                foreach ($tmp as $childId) {
+                    if($strictchilds)
+                        if(!in_array($childId, $strictchilds))
+                            continue;
+                    $pkey = (strlen($this->aliasListing[$childId]['path']) ? "{$this->aliasListing[$childId]['path']}/" : '') . $this->aliasListing[$childId]['alias'];
+                    if (!strlen($pkey)) $pkey = "{$childId}";
+                        $children[$pkey] = $childId;
 
-            foreach ($documentMap_cache[$id] as $childId) {
-                $pkey = (strlen($this->aliasListing[$childId]['path']) ? "{$this->aliasListing[$childId]['path']}/" : '') . $this->aliasListing[$childId]['alias'];
-                if (!strlen($pkey)) $pkey = "{$childId}";
-                    $children[$pkey] = $childId;
-
-                if ($depth) {
-                    $children += $this->getChildIds($childId, $depth);
+                    if ($depth || $depth < 0) {
+                        $children += $this->getChildIdsRec($childId, $depth, array(), $strictchilds);
+                    }
                 }
             }
+            return $children;
         }
-        return $children;
+    }else{ 
+
+        /**
+         * Returns an array of child IDs belonging to the specified parent.
+         *
+         * @param int $id The parent resource/document to start from
+         * @param int $depth How many levels deep to search for children, default: 10
+         * @param array $children Optional array of docids to merge with the result.
+         * @return array Contains the document Listing (tree) like the sitemap
+         */
+        function getChildIds($id, $depth= 10, $children= array ()) {
+
+            // Initialise a static array to index parents->children
+            static $documentMap_cache = array();
+            if (!count($documentMap_cache)) {
+                foreach ($this->documentMap as $document) {
+                    foreach ($document as $p => $c) {
+                        $documentMap_cache[$p][] = $c;
+                    }
+                }
+            }
+
+            // Get all the children for this parent node
+            if (isset($documentMap_cache[$id])) {
+                $depth--;
+
+                foreach ($documentMap_cache[$id] as $childId) {
+                    $pkey = (strlen($this->aliasListing[$childId]['path']) ? "{$this->aliasListing[$childId]['path']}/" : '') . $this->aliasListing[$childId]['alias'];
+                    if (!strlen($pkey)) $pkey = "{$childId}";
+                        $children[$pkey] = $childId;
+
+                    if ($depth) {
+                        $children += $this->getChildIds($childId, $depth);
+                    }
+                }
+            }
+            return $children;
+        }
     }
-
     /**
      * Displays a javascript alert message in the web browser
      *
