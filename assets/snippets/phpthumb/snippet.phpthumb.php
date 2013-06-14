0582ae0d63c8f4754fd6598185c11f522c2fa41b
Dmi3yy <dmi3yy@gmail.com>
2013-06-14 11:47:41 +0300

    fix phpthumb //

--- assets/snippets/phpthumb/snippet.phpthumb.php
+++ assets/snippets/phpthumb/snippet.phpthumb.php
@@ -3,7 +3,7 @@ if(!defined('MODX_BASE_PATH')){die('What are you doing? Get out of here!');}
 //[[phpthumb? &input=`[+image+]` &options=`w=150,h=76,far=C,bg=FFFFFF`]]
 //Author: Bumkaka
 
-$base="assets/cache/images/";   // тут можно менять путь
+$base="assets/cache/images";   // тут можно менять путь
 
 if(!defined('MODX_BASE_PATH')){die('What are you doing? Get out of here!');}
 
