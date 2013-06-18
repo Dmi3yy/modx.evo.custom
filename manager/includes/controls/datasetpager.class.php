16a48faa43696b974a62aa65173ef295d32fca71
Dmi3yy <dmi3yy@gmail.com>
2013-06-18 09:52:41 +0300

    #9849 Replace to DBAPI functions

--- manager/includes/controls/datasetpager.class.php
+++ manager/includes/controls/datasetpager.class.php
@@ -90,7 +90,7 @@ class DataSetPager {
 		if (!$this->selPageStyle) $this->selPageStyle = "font-weight:bold";
 		
 		// get total number of rows		
-		$tnr = ($isDataset)? mysql_num_rows($this->ds):count($this->ds); 
+		$tnr = ($isDataset)? $modx->db->getRecordCount($this->ds):count($this->ds);
 
 		// render: no records found
 		if($tnr<=0) {
@@ -143,7 +143,7 @@ class DataSetPager {
 			$fncObject = is_object($fnc);
 			$minitems = (($p-1)*$this->pageSize)+1;
 			$maxitems = (($p-1)*$this->pageSize)+$this->pageSize;
-			while ($i<=$maxitems && ($row = ($isDataset)? mysql_fetch_assoc($this->ds):$this->ds[$i-1])) {
+			while ($i<=$maxitems && ($row = ($isDataset)? $modx->db->getRow($this->ds):$this->ds[$i-1])) {
 				if ($i>=$minitems && $i<=$maxitems){
 					if($fncObject) {
 						if($args!="") $this->rows .= $fnc->RenderRowFnc($i,$row,$args);
