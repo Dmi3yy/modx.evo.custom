94424dfd051efeb07340d42d1aaabb424a98d635
Dmi3yy <dmi3yy@gmail.com>
2013-06-19 11:19:09 +0300

    fix multiTv js

--- assets/tvs/multitv/js/multitv.js
+++ assets/tvs/multitv/js/multitv.js
@@ -282,7 +282,7 @@ function SetUrl(url, width, height, alt) {
 			if (!field.hasClass('transformed')) {
 				// reset all event
 				fieldClear.find('a').click(function() {
-					var answer = confirm(tvlanguage.confirmclear);
+					var answer = confirm(settings.language.confirmclear);
 					if (answer) {
 						fieldList.children('li').remove();
 						field.val('[]');
@@ -593,8 +593,8 @@ function SetUrl(url, width, height, alt) {
 					data.settings.autoincrement = 1;
 				}
 				$.each(data.value, function(key, value) {
-					this.DT_RowId = key + 1;
-					this.MTV_RowId = key + 1;
+					this.DT_RowId = tvid + (key + 1);
+					this.MTV_RowId = (key + 1);
 				});
 			}
 
@@ -644,7 +644,7 @@ function SetUrl(url, width, height, alt) {
 						if (fieldInput.hasClass('image')) {
 							setThumbnail(value, fieldInput.attr('name'), fieldEditArea);
 						}
-						$('#' + tvid + key + '_mtv').setValue(value);
+						fieldInput.setValue(value);
 					});
 				} else {
 					fieldEditForm.find('.formtabradio:first').addClass('active').find('input[type="radio"]').attr('checked', 'checked');
@@ -700,14 +700,18 @@ function SetUrl(url, width, height, alt) {
 
 			// save/append edit box
 			function saveRow(mode) {
+				if (typeof tinyMCE != 'undefined') {
 				tinyMCE.triggerSave();
+				}
 				var values = new Object();
 				var saveTab = fieldEditForm.find('[name^="' + tvid + 'tab_radio_mtv"]').getValue();
 				values.fieldTab = (saveTab !== '') ? saveTab : '';
 				fieldEditArea.find(':input').each(function(i) {
-					var key = $(this).attr('name').replace(/tv.\d(.*)_mtv/, '$1');
+					if ($(this).attr('name')) {
+						var key = $(this).attr('name').replace(/tv\d+(.*)_mtv/, '$1');
 					if (key !== '') {
 						values[key] = $(this).val();
+					}
 					}
 				});
 				$.ajax({
@@ -731,7 +735,7 @@ function SetUrl(url, width, height, alt) {
 							fieldTable.fnUpdate(values, selected);
 						} else {
 							values.MTV_RowId = fieldTable.fnGetData().length + 1;
-							values.DT_RowId = fieldTable.fnGetData().length + 1;
+							values.DT_RowId = tvid + (fieldTable.fnGetData().length + 1);
 							fieldTable.fnAddData(values);
 						}
 						clearInputs(fieldEditArea);
