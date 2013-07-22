1c9c61a52ce79c85c4e3e0538dfb7ef1b1adae9e
Dmi3yy <dmi3yy@gmail.com>
2013-07-22 11:51:43 +0300

    fix evogallery widget for MGR_DIR

--- assets/plugins/managermanager/widgets/evogallery/evogallery.php
+++ assets/plugins/managermanager/widgets/evogallery/evogallery.php
@@ -23,7 +23,7 @@ function mm_widget_evogallery($moduleid, $title='', $roles='', $templates='') {
         
                 //TODO: Add iframe autoheight
                 if (isset($content['id']))
-                        $iframecontent = '<iframe id="mm_evogallery" src="'.$modx->config['site_url'].'manager/index.php?a=112&id='.$moduleid.'&onlygallery=1&action=view&content_id='.$content['id'].'" style="width:100%;height:600px;" scrolling="auto" frameborder="0"></iframe>';
+                        $iframecontent = '<iframe id="mm_evogallery" src="'.$modx->config['site_url'].MGR_DIR.'/index.php?a=112&id='.$moduleid.'&onlygallery=1&action=view&content_id='.$content['id'].'" style="width:100%;height:600px;" scrolling="auto" frameborder="0"></iframe>';
                 else
                         $iframecontent = '<p class="warning">'.$_lang['mm_save_required'].'</p>';
                 
