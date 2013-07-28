7d29da16d2c00c0630695a62d6580adeb7160c58
Dmi3yy <dmi3yy@gmail.com>
2013-07-28 11:26:45 +0300

    update version

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version = '1.0.10-d5.4Beta';           // Current version number
-$modx_release_date = 'Jun 21, 2013'; // Date of release
+$modx_version = '1.0.10-d5.4B2';           // Current version number
+$modx_release_date = 'Jul 28, 2013'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODx '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
