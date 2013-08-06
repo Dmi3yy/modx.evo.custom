d7c50839338836c860c73ae23db82b3de725c1b0
Dmi3yy <dmi3yy@gmail.com>
2013-08-07 01:45:41 +0300

    delete JotX

--- assets/snippets/jot/plugins/nolink.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-function nolink(&$object,$params){
-	global $modx;
-	
-	if ($params['name'] != 'email' && preg_match ('/href|url|http|www|\.ru|\.com|\.net|\.info|\.org/i', $params['value'])) return array(0,'Ссылки запрещены');
-
-}
-?>
