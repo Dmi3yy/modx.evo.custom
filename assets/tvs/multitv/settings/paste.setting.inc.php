8eccbb0fac98cfeee42e1e4e5b314da3553fd7d5
Dmi3yy <dmi3yy@gmail.com>
2013-08-22 15:50:40 +0300

    snippets to Extras Evolution

--- assets/tvs/multitv/settings/paste.setting.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$settings['css'] = array(
-	'[+tvpath+]css/colorbox.css'
-);
-$settings['scripts'] = array(
-	'[+tvpath+]js/jquery-colorbox-1.3.19.3-min.js',
-	'[+tvpath+]js/jquery-htmlClean-1.3.0.min.js'
-);
-?>
