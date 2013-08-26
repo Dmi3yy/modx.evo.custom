c9621841d3cb41e66916b9803d919856f9ebc71c
Dmi3yy <dmi3yy@gmail.com>
2013-08-26 19:21:38 +0300

    fix from 1.0.11

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -117,6 +117,14 @@ class DocumentParser {
                 else            return false;
                 break;
 
+            case 'EXPORT_SITE' :
+                if(include_once(MODX_BASE_PATH . 'manager/includes/extenders/export.class.inc.php'))
+                {
+                    $this->export= new EXPORT_SITE;
+                    return true;
+                }
+                else return false;
+                break;
             case 'PHPCOMPAT' :
             	if(is_object($this->phpcompat)) return;
                 include_once(MODX_BASE_PATH . 'manager/includes/extenders/phpcompat.class.inc.php');
