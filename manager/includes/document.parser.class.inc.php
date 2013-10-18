ae8bb0c5be8d31d92d47b599d472b0d386e9b38a
Dmi3yy <dmi3yy@gmail.com>
2013-10-18 20:23:15 +0300

    fix bug with  site_url and get

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -1328,8 +1328,12 @@ class DocumentParser {
                 $qstring = isset($url_query_string) ? preg_replace("#(^|&)(q|id)=[^&]+#", '', $url_query_string) : ''; // Strip conflicting id/q from query string
                 if ($qstring) $url = "{$site_url}?{$qstring}";
                 else          $url = $site_url;
-                $this->sendRedirect($url,0,'REDIRECT_HEADER', 'HTTP/1.0 301 Moved Permanently');
-                        exit(0);
+                if (empty($_POST)){
+	                if (('/?'.$qstring) != $_SERVER['REQUEST_URI']) {
+	                	$this->sendRedirect($url,0,'REDIRECT_HEADER', 'HTTP/1.0 301 Moved Permanently');
+	                	exit(0);
+	            	}
+            	}
                     }
         }elseif ($url_path != $strictURL && $this->documentIdentifier != $this->config['error_page']){
                      // Force page redirect
