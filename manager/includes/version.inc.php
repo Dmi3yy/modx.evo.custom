472b9ff62acabdb19cce8cb1f90d5920bf80aa22
Dmi3yy <dmi3yy@gmail.com>
2014-01-30 16:12:41 +0200

    update to 1.0.13RC3

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,5 +1,5 @@
 <?php
-$modx_version = '1.0.13RC2-d6.5';           // Current version number
-$modx_release_date = 'Jan 08, 2014'; // Date of release
+$modx_version = '1.0.13RC3-d6.5';           // Current version number
+$modx_release_date = 'Jan 30, 2014'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
