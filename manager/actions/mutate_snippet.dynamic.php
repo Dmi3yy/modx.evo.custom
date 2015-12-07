4f72e3b63c19015564aa419e8ec35162ed3512f4
ProjectSoft <projectsoft2009@yandex.ru>
2015-12-07 18:52:27 +0400

    Условие проверки list-multi

--- manager/actions/mutate_snippet.dynamic.php
+++ manager/actions/mutate_snippet.dynamic.php
@@ -104,7 +104,7 @@ function showParameters(ctrl) {
             value = decode((ar[2])? ar[2]:'');
 
             // store values for later retrieval
-            if (key && dt=='list') currentParams[key] = [desc,dt,value,ar[3]];
+            if (key && dt=='list' || dt=='list-multi') currentParams[key] = [desc,dt,value,ar[3]];
             else if (key) currentParams[key] = [desc,dt,value];
 
             if (dt) {
