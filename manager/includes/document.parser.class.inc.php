d65a4669e7f7bf5950cf62e29d65b99fe8cba6f6
dmi3yy <dmi3yy@gmail.com>
2016-01-25 16:43:55 +0200

    Get mergeDocumentContent from EVO 1.1RC2

--- manager/includes/document.parser.class.inc.php
+++ manager/includes/document.parser.class.inc.php
@@ -883,24 +883,64 @@ class DocumentParser {
     function mergeDocumentContent($content) {
         if (strpos($content, '[*') === false)
 			return $content;
-		$replace = array();
+        if(!isset($this->documentIdentifier)) return $content;
+        if(!isset($this->documentObject) || empty($this->documentObject)) return $content;
+        
 		$matches = $this->getTagsFromContent($content, '[*', '*]');
-		if ($matches) {
-			for ($i = 0; $i < count($matches[1]); $i++) {
-				if ($matches[1][$i]) {
-					$key = $matches[1][$i];
-					$key = substr($key, 0, 1) == '#' ? substr($key, 1) : $key; // remove # for QuickEdit format
-					$value = $this->documentObject[$key];
-					if (is_array($value)) {
-						include_once MODX_MANAGER_PATH . 'includes/tmplvars.format.inc.php';
-						include_once MODX_MANAGER_PATH . 'includes/tmplvars.commands.inc.php';
-						$value = getTVDisplayFormat($value[0], $value[1], $value[2], $value[3], $value[4]);
+        if(!$matches) return $content;
+        
+        foreach($matches[1] as $i=>$key) {
+            if(substr($key, 0, 1) == '#') $key = substr($key, 1); // remove # for QuickEdit format
+            if(strpos($key,'@')!==false) {
+                list($key,$str) = explode('@',$key,2);
+                $context = strtolower($str);
+                if(substr($str,0,5)==='alias' && strpos($str,'(')!==false)
+                    $context = 'alias';
+                elseif(substr($str,0,1)==='u' && strpos($str,'(')!==false)
+                    $context = 'uparent';
+                switch($context) {
+                    case 'site_start':
+                        $docid = $this->config['site_start'];
+                        break;
+                    case 'parent':
+                    case 'p':
+                        $docid = $this->documentObject['parent'];
+                        if($docid==0) $docid = $this->config['site_start'];
+                        break;
+                    case 'ultimateparent':
+                    case 'uparent':
+                    case 'up':
+                    case 'u':
+                        if(strpos($str,'(')!==false) {
+                            $top = substr($str,strpos($str,'('));
+                            $top = trim($top,'()"\'');
 					}
-					$replace[$i] = $value;
+                        else $top = 0;
+                        $docid = $this->getUltimateParentId($this->documentIdentifier,$top);
+                        break;
+                    case 'alias':
+                        $str = substr($str,strpos($str,'('));
+                        $str = trim($str,'()"\'');
+                        $docid = $this->getIdFromAlias($str);
+                        break;
+                    default:
+                        $docid = $str;
+                }
+                if(preg_match('@^[1-9][0-9]*$@',$docid))
+                    $value = $this->getField($key,$docid);
+                else $value = '';
 				}
+            elseif(!isset($this->documentObject[$key])) $value = '';
+            else $value= $this->documentObject[$key];
+            
+            if (is_array($value)) {
+                include_once(MODX_MANAGER_PATH . 'includes/tmplvars.format.inc.php');
+                include_once(MODX_MANAGER_PATH . 'includes/tmplvars.commands.inc.php');
+                $value = getTVDisplayFormat($value[0], $value[1], $value[2], $value[3], $value[4]);
 			}
-			$content = str_replace($matches[0], $replace, $content);
+            $content= str_replace($matches['0'][$i], $value, $content);
 		}
+        
 		return $content;
 	}
 
