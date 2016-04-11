1ab7281692bbcf4dfb449e14066355d0ec740d12
dmi3yy <dmi3yy@gmail.com>
2016-04-11 14:17:49 +0300

    no message

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-$modx_version = '1.1-d8';           // Current version number
+$modx_version = '1.1-d8.0';           // Current version number
 $modx_release_date = 'Apr 11, 2016'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
