71c334a14fd5659e93e682ba72d4d660b97f3e84
Dmytro Lukianenko <dmi3yy@gmail.com>
2016-09-30 21:38:41 +0300

    fix store.js

--- assets/modules/store/js/store.js
+++ assets/modules/store/js/store.js
@@ -28,11 +28,7 @@ function store_search(val){
 	})
 }	
 
-/*MUST DELETED*/
 
-var admin_send = '';
-
-/* ========== */
 
 
 	
@@ -210,7 +206,7 @@ store = {
 	query:function(action,param,callback){
 		param = store.extend(param);
 		$.ajax({
-			url:'http://extras.evolution-cms.com/get.php?get=' + action+admin_send,
+			url:'http://extras.evolution-cms.com/get.php?get=' + action,
 			cache:false,
 			data:param,
 			dataType: "json",
