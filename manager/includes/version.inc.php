487c1ad8d168ed3b3f7b5687c8687e4a0ce38f69
Dmytro Lukianenko <dmi3yy@gmail.com>
2016-10-15 14:05:52 +0300

    up

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,6 +1,6 @@
 <?php
 
 $modx_version = '1.1-d8.1.4';           // Current version number
-$modx_release_date = 'Oct 14, 2016'; // Date of release
+$modx_release_date = 'Oct 15, 2016'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
-$modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
+$modx_full_appname = "MODX {$modx_branch} {$modx_version} ({$modx_release_date})";
