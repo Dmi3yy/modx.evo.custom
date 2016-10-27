9e1010f2f63d1a433043670b1e625832350cc8d8
Dmytro Lukianenko <dmi3yy@gmail.com>
2016-10-27 21:07:10 +0300

    fix from off version

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,6 +1,6 @@
 <?php
 
 $modx_version = '1.2-d8.1.4';           // Current version number
-$modx_release_date = 'Oct 21, 2016'; // Date of release
+$modx_release_date = 'Oct 27, 2016'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = "MODX {$modx_branch} {$modx_version} ({$modx_release_date})";
