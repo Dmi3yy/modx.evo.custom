ab466848b01fc53cbd50bcd8ee5d8a257907536e
Dmytro Lukianenko <dmi3yy@gmail.com>
2016-12-14 14:26:07 +0200

    1.2-d9.0.3 fix nested snippets   and turn off by default elementsInTree plugin.

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-$modx_version = '1.2-d9.0.2';           // Current version number
-$modx_release_date = 'Dec 13, 2016'; // Date of release
+$modx_version = '1.2-d9.0.3';           // Current version number
+$modx_release_date = 'Dec 14, 2016'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = "MODX {$modx_branch} {$modx_version} ({$modx_release_date})";
