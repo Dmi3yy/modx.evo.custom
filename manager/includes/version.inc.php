c51f44c4100a7dfb0967f8c8dc1ddb8b44050346
dmi3yy <dmi3yy@gmail.com>
2016-12-26 14:00:53 +0200

    Update to 1.2-d9.0.5

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-$modx_version = '1.2-d9.0.4';           // Current version number
-$modx_release_date = 'Dec 16, 2016'; // Date of release
+$modx_version = '1.2-d9.0.5';           // Current version number
+$modx_release_date = 'Dec 26, 2016'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
 $modx_full_appname = "MODX {$modx_branch} {$modx_version} ({$modx_release_date})";
