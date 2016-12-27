80f0c0ff713c4bddc48a4bd46fe0010d40e040cc
dmi3yy <dmi3yy@gmail.com>
2016-12-27 13:12:07 +0200

    Update ElementsTree, MultiSession, oh-mailer

--- install/setup.sql
+++ install/setup.sql
@@ -7,24 +7,26 @@
 DROP TABLE IF EXISTS `{PREFIX}active_users`;
 
 CREATE TABLE `{PREFIX}active_users` (
+  `sid` varchar(32) NOT NULL default '',
   `internalKey` int(9) NOT NULL default '0',
   `username` varchar(50) NOT NULL default '',
   `lasthit` int(20) NOT NULL default '0',
   `action` varchar(10) NOT NULL default '',
   `id` int(10) default NULL,
-  PRIMARY KEY  (`internalKey`)
+  PRIMARY KEY (`sid`)
 ) ENGINE=MyISAM COMMENT='Contains data about last user action.';
 
 DROP TABLE IF EXISTS `{PREFIX}active_user_locks`;
 
 CREATE TABLE `{PREFIX}active_user_locks` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
+  `sid` varchar(32) NOT NULL default '',
   `internalKey` int(9) NOT NULL default '0',
   `elementType` int(1) NOT NULL default '0',
   `elementId` int(10) NOT NULL default '0',
   `lasthit` int(20) NOT NULL default '0',
   PRIMARY KEY(`id`),
-  UNIQUE INDEX ix_element_id (`internalKey`,`elementType`,`elementId`)
+  UNIQUE INDEX ix_element_id (`elementType`,`elementId`,`sid`)
 ) ENGINE=MyISAM COMMENT='Contains data about locked elements.';
 
 DROP TABLE IF EXISTS `{PREFIX}active_user_sessions`;
